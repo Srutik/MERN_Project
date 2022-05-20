b75bbde1a1f9859a8a8a43c1b928b4861dc7989a
Srutik Borda <srutikborda266@gmail.com>
2022-05-20 16:21:27 +0530

    add

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MERN_Project
 
 -- Adding Cookies while registration and expires after 5sec.
+
+-- Adding auth as middleware and authenticate any secret page ( Not login without Login).
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 const hbs = require("hbs");
 const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const auth = require("./middleware/auth");
 require("./db/conn");
 
 const Register = require("./models/registers");
@@ -15,7 +17,8 @@ const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
@@ -24,15 +27,36 @@ hbs.registerPartials(partials_path);
 
 // console.log(process.env.SECRET_KEY);
 
-app.get("/", (req , res) => {
+app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/register", (req , res) => {
+app.get("/secret", auth, (req, res) => {
+    // console.log(`this is the cookie ${req.cookies.jwt}`)
+    res.render("secret");
+});
+
+app.get("/logout", auth, async (req, res) => {
+    try {
+
+        req.user.tokens = req.user.tokens.filter((currentElement) => {
+            return currentElement.token != req.token
+        })
+        res.clearCookie("jwt");
+        console.log("logout sucessfull");
+
+        await req.user.save();
+        res.render("login");
+    } catch (error) {
+        res.status(401).send(error);
+    }
+});
+
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", async (req , res) => {
+app.post("/register", async (req, res) => {
     try {
         const password = req.body.password;
         const cpassword = req.body.cpassword;
@@ -52,9 +76,9 @@ app.post("/register", async (req , res) => {
             const token = await registerEmployee.generateAuthToken();
             console.log("the token part : " + token);
 
-            res.cookie("jwt" , token , {
-                expires:new Date(Dsate.now() + 50000),
-                httpOnly:true
+            res.cookie("jwt", token, {
+                expires: new Date(Dsate.now() + 60000),
+                httpOnly: true
             });
 
             console.log(cookie);
@@ -81,14 +105,20 @@ app.post("/login", async (req, res) => {
 
         const userEmail = await Register.findOne({ email: email });
 
-        const isMatch = await bcrypt.compare(password , userEmail.password);
+        const isMatch = await bcrypt.compare(password, userEmail.password);
 
         const token = await userEmail.generateAuthToken();
         console.log("the token part : " + token);
 
-        if(isMatch) {
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 60000),
+            httpOnly: true,
+            // secure:true
+        });
+
+        if (isMatch) {
             res.status(201).render("home")
-        }else {
+        } else {
             res.send("Invalid login Details.")
         }
     } catch (error) {
